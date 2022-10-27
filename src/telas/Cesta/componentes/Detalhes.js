210904d078cf2644d62ec3ab487c2004c3445901
gabriel.oliveira <gabriel.oliveira@pamafa.com.br>
2022-10-27 16:10:40 -0300

    finalização das aulas

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -3,9 +3,11 @@ import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 
 import useTextos from '../../../hooks/useTextos';
 import Texto from '../../../componentes/Texto';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Detalhes({ nome, produtor, descricao, preco }) {
   const { botaoComprar } = useTextos();
+  const navigation = useNavigation();
 
   return <>
     <Texto style={estilos.nome}>{ nome }</Texto>
@@ -18,7 +20,7 @@ export default function Detalhes({ nome, produtor, descricao, preco }) {
 
     <TouchableOpacity 
       style={estilos.botao} 
-      onPress={() => {}}>
+      onPress={() => {navigation.navigate('HomeScreen',{compra:{nome, timestamp: + new Date() }})}}>
       <Texto style={estilos.textoBotao}>{ botaoComprar }</Texto>
     </TouchableOpacity>
   </>
