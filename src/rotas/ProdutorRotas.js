d9efe5ae65907fbd3375d721e5b6a3e7fbf7fa80
gabriel.oliveira <gabriel.oliveira@pamafa.com.br>
2022-10-27 14:12:00 -0300

    Finalização da tela de Produtor

---
+++ src/rotas/ProdutorRotas.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+
+import Home from "../telas/Home";
+import Produtor from "../telas/Produtor";
+
+const Stack = createNativeStackNavigator();
+
+export default function ProdutorRotas({ ComponentPrincipal = Home }) {
+    return <Stack.Navigator>
+        <Stack.Screen name='HomeScreen' component={ComponentPrincipal} />
+        <Stack.Screen name='Produtor' component={Produtor} />
+    </Stack.Navigator>
+}
--- src/telas/MelhoresProdutores.js
+++ src/telas/MelhoresProdutores.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Produtores from './Produtores';
 
-export default function melhoresProdutores(){
+export default function MelhoresProdutores(){
     return <Produtores melhoresProdutores={true} />
 }
